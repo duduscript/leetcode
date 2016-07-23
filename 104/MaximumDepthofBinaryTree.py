1c5c69631a5c9c14d7e26b568a5b2adff6007f69
duduscript <duduscript@outlook.com>
2016-07-23 22:21:37 +0800

    104

---
+++ 104/MaximumDepthofBinaryTree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return 0 if root == None else 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
